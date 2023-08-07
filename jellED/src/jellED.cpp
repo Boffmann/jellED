cf41da4aa16492863c4a9134cffa7bfbfad677c0
Hendrik Tjabben <hendrik.tjabben@student.hpi.de>
2023-08-07 17:12:55 +0200

    Adds Logic Level Shifter
    Adds 74hct125 as logic level shifter to shift 3.3V output of the ESP32
    pin to 5V for the WS2812.
    
    Further adds KiCad schematic for wiring.

--- jellED/src/jellED.cpp
+++ jellED/src/jellED.cpp
@@ -1,13 +1,16 @@
 #include "WS2812.h"
 
-constexpr uint8_t LED_PIN = 15;
+constexpr uint8_t LED_PIN = 13;
+constexpr uint8_t LED_ENABLE = 14;
 constexpr uint16_t NUM_LEDS = 2;
 
 WS2812 strip = WS2812(LED_PIN, NUM_LEDS);
  
 void setup() {
+   pinMode(LED_ENABLE, OUTPUT);
    strip.initialize();
    strip.setBrightness(128);
+   digitalWrite(LED_ENABLE, LOW);
 }
  
 void loop() {
