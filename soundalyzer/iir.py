5e1893df13c9a739e1ee9092749491bfa0f25000
Hendrik Tjabben <hendrik.tjabben@gmail.com>
2024-06-10 11:49:38 +0200

    Adds Bandpass

--- soundalyzer/iir.py
+++ soundalyzer/iir.py
@@ -14,6 +14,12 @@ def filter_lowpass_scipy(wave: Wave):
     filtered = signal.sosfilt(sos, ys)
     return Wave(filtered, wave.ts, wave.framerate)
 
+def filter_bandpass_scipy(wave: Wave):
+    sos = signal.butter(order, [20, 100], 'band', fs=sample_rate, output='sos')
+    ys = wave.ys
+    filtered = signal.sosfilt(sos, ys)
+    return Wave(filtered, wave.ts, wave.framerate)
+
 def filter_lowpass_ab_scipy(wave: Wave):
     b, a = signal.butter(order, lowcut, 'low', fs=sample_rate)
     filtered = signal.filtfilt(b, a, wave.ys)
@@ -48,18 +54,6 @@ def iir_filter(b, a, sig):
     for n in range(len(sig)):
         filtered_signal[n] = iir_filter_sample_order_2(b, a, sig[n])
     return filtered_signal
-        # sum_b = 0
-        # sum_a = 0
-        # for i in range(order):
-        #     sig_value = 0.0 if n - i < 0 else sig[n - i]
-        #     sum_b += b[i] * sig_value
-        # for j in range(1, order):
-        #     sig_value = 0.0 if n - j < 0 else filtered_signal[n - j]
-        #     sum_a += a[j] * sig_value
-
-        # filtered_signal[n] = sum_b - sum_a
-        # filtered_signal[n] *= (1.0 / a[0])
-    # return filtered_signal
 
 
 def filter_lowpass_ab_own(wave: Wave):
@@ -68,58 +62,20 @@ def filter_lowpass_ab_own(wave: Wave):
     filtered_2 = iir_filter(b, a, filtered[::-1])
     return Wave(filtered_2[::-1], wave.ts, wave.framerate)
 
-def filter_lowpass_own(wave: Wave):
-    sos = [[ 1.80397952e-09, 3.60795904e-09, 1.80397952e-09, 1.00000000e+00,
-            -1.97593328e+00, 9.76102578e-01],
-            [ 1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00,
-            -1.98986110e+00, 9.90031591e-01]]
-    # sos = signal.butter(order, lowcut, 'low', fs=sample_rate, output='sos')
+def filter_bandpass_own(wave: Wave):
+    sos = [[ 7.41426642e-10,  1.48285328e-09,  7.41426642e-10,  1.00000000e+00,
+        -1.98596111e+00,  9.86051254e-01],
+        [ 1.00000000e+00,  2.00000000e+00,  1.00000000e+00,  1.00000000e+00,
+        -1.99324771e+00,  9.93408841e-01],
+        [ 1.00000000e+00, -2.00000000e+00,  1.00000000e+00,  1.00000000e+00,
+        -1.99469814e+00,  9.94711042e-01],
+        [ 1.00000000e+00, -2.00000000e+00,  1.00000000e+00,  1.00000000e+00,
+        -1.99859190e+00,  9.98599159e-01]]
+    # sos = signal.butter(order, [20, 100], 'band', fs=sample_rate, output='sos')
     filtered_signal = wave.ys
-    for section in range(2):
+    for section in range(len(sos) - 1):
         b = [sos[section][0], sos[section][1], sos[section][2]]
         a = [sos[section][3], sos[section][4], sos[section][5]]
         filtered_signal = iir_filter(b, a, filtered_signal)
     return Wave(filtered_signal, wave.ts, wave.framerate)
-
-    # ys = np.zeros_like(wave.ts)
-    # # z = [[0, 0], [0, 0]]
-    # # z = np.zeros((sos.shape[0], 2))
-    # result = []
-    # intermediate = []
-    # # for section in range(2):
-    # section = 0
-    # b = [sos[section][0], sos[section][1], sos[section][2]]
-    # a = [sos[section][3], sos[section][4], sos[section][5]]
-    # for n in range(len(wave.ts)):
-    #     b_sum = 0
-    #     a_sum = 0
-    #     for i in range(2):
-    #         if n - i < 0:
-    #             continue
-    #         b_sum += b[i] * sig[n - i]
-    #     for j in range(1, 2, 1):
-    #         if n - j < 0:
-    #             continue
-    #         b_sum += a[j] * sig[n - j]
-
-    #     intermediate.append((1.0 / a[0]) * (b_sum - a_sum))
-
-    # section = 1
-    # b = [sos[section][0], sos[section][1], sos[section][2]]
-    # a = [sos[section][3], sos[section][4], sos[section][5]]
-    # for n in range(len(wave.ts)):
-    #     b_sum = 0
-    #     a_sum = 0
-    #     for i in range(2):
-    #         if n - i < 0:
-    #             continue
-    #         b_sum += b[i] * intermediate[n - i]
-    #     for j in range(1, 2, 1):
-    #         if n - j < 0:
-    #             continue
-    #         b_sum += a[j] * intermediate[n - j]
-
-    #     result.append((1.0 / a[0]) * (b_sum - a_sum))
-
-    # return Wave(result, wave.ts, wave.framerate)
 
--- soundalyzer/soundalyzer.py
+++ soundalyzer/soundalyzer.py
@@ -12,12 +12,12 @@ def soundalyzer_main():
     wave = read_wave("/Users/tjabben/Documents/techno-drums-loop-120-bpm-monno.wav")
     # wave = read_wave("/Users/tjabben/repos/jellED/soundalyzer/sound.wav")
     main.plot(plot_index, wave.ts, wave.ys)
-    filtered_wave = iir.filter_lowpass_scipy(wave)
-    main.plot(plot_index_2, filtered_wave.ts, filtered_wave.ys)
-    filtered_own = iir.filter_lowpass_own(wave)
+    # filtered_wave = iir.filter_bandpass_scipy(wave)
+    # main.plot(plot_index_2, filtered_wave.ts, filtered_wave.ys)
+    filtered_own = iir.filter_bandpass_own(wave)
     main.plot(plot_index_3, filtered_own.ts, filtered_own.ys)
-    main.start_playing_wave()
-    filtered_own.play()
+    # main.start_playing_wave()
+    # filtered_own.play()
     # wave.play()
 
 if __name__ == "__main__":
