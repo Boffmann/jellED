958ba08cce83e05ea7cd2d6c3325effadd05f690
Hendrik Tjabben <hendrik.tjabben@gmail.com>
2024-06-14 00:34:24 +0200

    Adds simple peak detection
    TODO Needs finetuning and documentation

---
+++ soundalyzer/peakdetection.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+
+class PeakDetector:
+    def __init__(self, lag, threshold, influence):
+        self.y = list()
+        self.length = len(self.y)
+        self.lag = lag
+        self.threshold = threshold
+        self.influence = influence
+        self.signals = [0] * len(self.y)
+        self.filteredY = np.array(self.y).tolist()
+        self.avgFilter = [0] * len(self.y)
+        self.stdFilter = [0] * len(self.y)
+        # self.avgFilter[self.lag - 1] = np.mean(self.y[0:self.lag]).tolist()
+        # self.stdFilter[self.lag - 1] = np.std(self.y[0:self.lag]).tolist()
+
+    def thresholding_algo(self, new_value):
+        self.y.append(new_value)
+        i = len(self.y) - 1
+        self.length = len(self.y)
+        if i < self.lag:
+            return 0
+        elif i == self.lag:
+            self.signals = [0] * len(self.y)
+            self.filteredY = np.array(self.y).tolist()
+            self.avgFilter = [0] * len(self.y)
+            self.stdFilter = [0] * len(self.y)
+            self.avgFilter[self.lag] = np.mean(self.y[0:self.lag]).tolist()
+            self.stdFilter[self.lag] = np.std(self.y[0:self.lag]).tolist()
+            return 0
+
+        self.signals += [0]
+        self.filteredY += [0]
+        self.avgFilter += [0]
+        self.stdFilter += [0]
+
+        # print("V1: " + str(abs(self.y[i] - self.avgFilter[i - 1])) + " V2: " + str(self.threshold * self.stdFilter[i - 1]))
+        # print("V1: " + str(self.avgFilter[i - 1]) + " V2: " + str(self.stdFilter[i - 1]))
+
+        if abs(self.y[i] - self.avgFilter[i - 1]) > (self.threshold * self.stdFilter[i - 1]):
+
+            if self.y[i] > self.avgFilter[i - 1]:
+                self.signals[i] = 1
+            else:
+                self.signals[i] = 0 # -1
+
+            self.filteredY[i] = self.influence * self.y[i] + \
+                (1 - self.influence) * self.filteredY[i - 1]
+            self.avgFilter[i] = np.mean(self.filteredY[(i - self.lag):i])
+            self.stdFilter[i] = np.std(self.filteredY[(i - self.lag):i])
+        else:
+            self.signals[i] = 0
+            self.filteredY[i] = self.y[i]
+            self.avgFilter[i] = np.mean(self.filteredY[(i - self.lag):i])
+            self.stdFilter[i] = np.std(self.filteredY[(i - self.lag):i])
+
+
+        return self.signals[i]
+
+
+# class PeakDetector():
+#     def __init__(self, lag, threshold, influence):
+#         self.lag = lag
+#         self.threshold = threshold
+#         self.influence = influence
+
+#         self.index = 0
+
+#         self.data = [] * lag + 1
+#         self.avg = [] * lag + 1
+#         self.std = [] * lag + 1
+
+#     def getAvg(start, len):
+#         x = 0.0;
+#         for i in range(len):
+#             x += self.data[(start + i) % self.lag];
+#         return x / len;
+
+#     def thresholding_algo(self, new_value):
+#         i = self.index % self.lag
+#         j = (self.index + 1) % self.lag
+#         deviation = new_value - self.avg[i]
+
+#         if deviation > self.threshold * self.std[i]:
+#             self.data[j] = self.influence * new_value + (1.0 - self.influence) * self.data[i]
+#             self.peak = 1
+#         elif deviation < -self.threshold * self.std[i]:
+#             self.data[j] = self.influence * new_value + (1.0 - self.influence) * self.data[i]
+#             self.peak = -1
+#         else:
+#             self.data[j] = new_value
+
+#         self.avg[i] = getAvg(j, self.lag)
+#         self.std[j] = getStd(j, self.lag)
+
+
+
+        # peak = 0;
+        # int i = index % lag; //current index
+        # int j = (index + 1) % lag; //next index
+        # double deviation = newSample - avg[i];
+        # if (deviation > threshold * std[i]) {
+        #     data[j] = influence * newSample + (1.0 - influence) * data[i];
+        #     peak = 1;
+        # }
+        # else if (deviation < -threshold * std[i]) {
+        #     data[j] = influence * newSample + (1.0 - influence) * data[i];
+        #     peak = -1;
+        # }
+        # else
+        #     data[j] = newSample;
+        # avg[j] = getAvg(j, lag);
+        # std[j] = getStd(j, lag);
+        # index++;
+        # if (index >= 16383) //2^14
+        #     index = lag + j;
+        # return(std[j]);
--- soundalyzer/soundalyzer.py
+++ soundalyzer/soundalyzer.py
@@ -5,6 +5,7 @@ from mainwindow import MainWindow
 from wav import Wave, read_wave
 from bandpass import BandpassFilter
 from envelope import EnvelopeDetector
+from peakdetection import PeakDetector
 import math
 import filter
 from PyQt5 import QtCore, QtWidgets
@@ -16,24 +17,40 @@ def soundalyzer_main():
     main.plot(plot_index, wave.ts, wave.ys, color=(0, 0, 0))
     bandpass = BandpassFilter(4, 20, 100, wave.framerate)
     envelopeDetector = EnvelopeDetector(1024, 512)
+    peakDetector = PeakDetector(10, 2, 0.5)
 
     bandpass_filtered = []
     envelope = []
+    peaks = []
     now = datetime.datetime.now()
+    prev_env_value = -1
+    first = True
     for sample in wave.ys:
         filtered_sample = bandpass.iir_filter_sos(sample)
         bandpass_filtered.append(filtered_sample)
-        envelope.append(envelopeDetector.envelope_sample(filtered_sample))
+        enveloped_sample = envelopeDetector.envelope_sample(filtered_sample)
+        if prev_env_value != enveloped_sample or first:
+            prev_env_value = enveloped_sample
+            envelope.append(enveloped_sample)
+            first = False
+            peaks.append(peakDetector.thresholding_algo(enveloped_sample))
+
     print("Calculating with custom implementation took: " + str(datetime.datetime.now() - now))
     main.plot(plot_index_2, wave.ts, bandpass_filtered)
-    main.plot(plot_index_2, wave.ts, envelope, color=(0, 0, 255))
+    ts = np.arange(0, wave.ts[-1], wave.ts[-1] / len(peaks))
+    # main.plot(plot_index_2, wave.ts, envelope, color=(0, 0, 255))
+    # main.plot(plot_index_3, wave.ts, envelope, color=(0, 255, 0))
+    # main.plot(plot_index_3, wave.ts, peaks, color=(255, 0, 255))
+    main.plot(plot_index_2, ts, envelope, color=(0, 0, 255))
+    main.plot(plot_index_3, ts, envelope, color=(0, 255, 0))
+    main.plot(plot_index_3, ts, peaks, color=(255, 0, 255))
 
-    now = datetime.datetime.now()
-    filtered = filter.filter_bandpass(wave, lowcut=20, highcut=100, order=4)
-    ts, ys = envelopeDetector.filter_envelope_backward(filtered)
-    print("Calculating with scipy+numpy took: " + str(datetime.datetime.now() - now))
-    main.plot(plot_index_3, filtered.ts, filtered.ys, color=(0, 0, 255))
-    main.plot(plot_index_3, ts, ys, color=(255, 0, 0))
+    # now = datetime.datetime.now()
+    # filtered = filter.filter_bandpass(wave, lowcut=20, highcut=100, order=4)
+    # ts, ys = envelopeDetector.filter_envelope_backward(filtered)
+    # print("Calculating with scipy+numpy took: " + str(datetime.datetime.now() - now))
+    # main.plot(plot_index_3, filtered.ts, filtered.ys, color=(0, 0, 255))
+    # main.plot(plot_index_3, ts, ys, color=(255, 0, 0))
 
     # main.start_playing_wave()
     # filtered_own.play()
